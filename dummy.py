cd52892735745658d33367e5d8b9a76fbb0e67c2
procrastinator <ihrishikeshnate@gmail.com>
2024-02-26 09:14:00 +0530

    Replace secrets with placeholder values

--- dummy.py
+++ dummy.py
@@ -1,14 +1,14 @@
 # Example Python script with hardcoded sensitive information
 
 # Hardcoded email and password (not secure!)
-email = "user@example.com"
-password = "SuperSecretPassword123!"
+email = "SECRET_PLACEHOLDER_75cb5c29708e477cbc42af921692798e"
+SECRET_PLACEHOLDER_41e85f031e5845c8a24baa1487ede456
 
 # Hardcoded API token (risk of exposure!)
-api_token = "123456789abcdef123456789aaaaasss"
+api_SECRET_PLACEHOLDER_a18e89bd053d4779968d2009894c66de
 
 # Hardcoded private key (major security vulnerability!)
-private_key = """-----BEGIN RSA PRIVATE KEY-----
+private_key = """SECRET_PLACEHOLDER_69b0af77801b42848d734813eba47ff5
 MIICXgIBAAKBgQCqGKukO1De7Zhrt4Ga3F0skO1De7Zhrt4Ga3F0skO1De7Zhrt4
 Ga3F0skO1De7Zhrt4Ga3F0skO1De7Zhrt4Ga3F0skO1De7Zhrt4Ga3F0skO1De7Z
 hrt4Ga3F0skO1De7Zhrt4Ga3F0skO1De7Zhrt4Ga3F0skO1De7Zhrt4Ga3F0sIDAQABAoGB
